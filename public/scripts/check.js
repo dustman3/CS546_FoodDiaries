21bddbaf497d3079ee3797b34f75b735417057d7
hyrongBruce <45904403+hyrongBruce@users.noreply.github.com>
2018-12-16 06:12:41 +0800

    public

---
+++ public/scripts/check.js
@@ -0,0 +1,41 @@
+// function InputSubmit (e) {
+//     if(!document.getElementsByName('recipesName')[0].value) {
+//         alert('Please enter your username and password')
+//         e.preventDefault()
+//     }
+//     return true
+// }
+// //prevent Default
+
+// var input = document.getElementById("phrase-label")
+// var button = document.querySelector(".submit-button")
+// var form = document.querySelector(".form")
+
+// var regEx = /[^a-zA-Z0-9]/g
+
+// //check if the USER input valid text, if null then can't submit
+// function check(){
+//     if(input.value.trim() !== ""){
+//         button.style.backgroundColor = "#fa923f";
+//         button.disabled = false;
+//     }else{
+//         button.style.backgroundColor = "#979695";
+//         button.disabled = true;
+//     }
+// }
+
+// form.addEventListener('submit', function(event){
+//     event.preventDefault();
+// })
+// var input = document.getElementById("search-input")
+
+// // var regEx = /[^a-zA-Z0-9]/g
+
+// input.addEventListener('keypress', checkTitle, false) 
+// function checkTitle(evt) {
+//     if(evt.value.trim() == ""){
+//         evt.preventDefault();
+//         alert("Please input TITLE NAME instead of empty")
+//     }
+// }
+
